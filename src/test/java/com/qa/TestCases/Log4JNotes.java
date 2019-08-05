79b3f7d6b9a47c29cf5dede718e49e35448c3f76
pavankrishnan77 <pavankrishnan1993@gmail.com>
2019-08-05 23:54:58 +0530

    Modified

--- src/test/java/com/qa/TestCases/Log4JNotes.java
+++ src/test/java/com/qa/TestCases/Log4JNotes.java
@@ -5,10 +5,10 @@ public class Log4JNotes
 	//Logs are used to know what is happening in every line at the time of execution or at Run Time.
 	
 	//Levels of Logs:
-	//1. Info
-	//2. Warn
-	//3. Error
-	//4. Fatal
+	//1. Info.
+	//2. Warn.
+	//3. Error.
+	//4. Fatal.
 	
 	//To Generate the Logs, we use Apache Lo4J API. [We have added Log4J Dependency into Pom.xml].
 	//It works by reading Log4J Configuration from Log4J.properties file.
